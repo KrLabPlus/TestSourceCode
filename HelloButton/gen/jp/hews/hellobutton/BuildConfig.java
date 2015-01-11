0e5410e92a00b5bca6489b1a21084bf11442aafd
hideki-yoshikawa <gamania1527@gmail.com>
2015-01-11 22:14:53 +0900

    サンプルファイルの削除

--- HelloButton/gen/jp/hews/hellobutton/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.hews.hellobutton;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- RerativeLayoutSample/src/jp/hews/hellobutton/R.java
+++
@@ -1,5 +0,0 @@
-package jp.hews.hellobutton;
-
-public class R {
-
-}
