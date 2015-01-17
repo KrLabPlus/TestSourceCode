8ce2e036f3f9468730c6c6d1a7842ba682c26a5a
okuharu <meteoricsky0122@gmail.com>
2015-01-17 17:44:29 +0900

    クレジット文章入り
    画像入れるの(私の力量不足で)時間かかりそうだったので、文章Onlyです。

--- Credit/src/com/example/test/MainActivity.java
+++ Credit/src/com/example/test/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.test;
 
+
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -25,11 +29,16 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+
+        
 		switch (item.getItemId()) {
 		case R.id.menu_credit:
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	    builder.setTitle("クレジット");
-	    builder.setMessage("ダイアログのメッセージ");
+	    builder.setTitle("このアプリについて");
+	    builder.setMessage("Rometta! ver 1.0.1" + "\n" +"現在このアプリケーションは最新です。" + "\n" +"\n" 
+	    		+ "Rometta!はとさでん交通株式会社が運営する高知県の路面電車専用の乗り換えアプリケーションです。"
+	    		+ "このアプリケーションは以下のAPIを使用しています。"+ "\n" + "\n" + "Google Maps API"
+	    		+ "\n" + "ぐるなびAPI" + "\n" + "Yahoo! Open Local Platform");
 	    AlertDialog dialog = builder.create();
 	    dialog.show();
 	    return true;
